a6e033f451f2d6666d608e70804641e3dec826a6
DarkProrokk <Seroshtanmakcs@gmail.com>
2025-03-19 11:23:44 +1000

    add results package

---
+++ Results/OperationResult.cs
@@ -0,0 +1,16 @@
+namespace Results;
+
+public class OperationResult
+{
+    public bool IsSuccess { get; }
+    public string Message { get; }
+
+    public OperationResult(bool isSuccess, string message)
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+    }
+
+    public static OperationResult Success() => new(true, "Operation complete successfully.");
+    public static OperationResult Failure(string errorMessage) => new(false, errorMessage);
+}
