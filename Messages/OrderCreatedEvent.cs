abe98fcecdf7dbaed017f6eaa917a8eb0aeffce8
DarkProrokk <Seroshtanmakcs@gmail.com>
2025-03-24 16:50:09 +1000

    change type of Item Reference

--- Messages/OrderCreatedEvent.cs
+++ Messages/OrderCreatedEvent.cs
@@ -3,7 +3,7 @@ namespace Messages;
 public class OrderCreatedEvent
 {
     public Guid OrderReference { get; set; }
-    public List<Guid> ItemsReference { get; set; } = null!;
+    public Dictionary<Guid, int> ItemsReference { get; set; } = null!;
 
     public bool Validate()
     {
