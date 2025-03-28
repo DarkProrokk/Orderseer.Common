0d6b60c5f31ca01245d68c408816b237daedffac
DarkProrokk <Seroshtanmakcs@gmail.com>
2025-03-28 11:07:46 +1000

    add order mapper extensions

---
+++ Extensions/OrderMapper.cs
@@ -0,0 +1,42 @@
+﻿using Orderseer.Common.Entities;
+using Orderseer.Common.Models.Dto;
+
+namespace Orderseer.Common.Extensions;
+
+public static class OrderMapper
+{
+    public static CreateOrderDto ToCreateOrderDto(this Order order)
+    {
+        return new CreateOrderDto
+        {
+            UserReference = order.UserId
+        };
+    }
+
+    public static DeliveryDto ToDeliveryDto(this Order order)
+    {
+        return new DeliveryDto
+        {
+            OrderId = order.OrderId,
+            DeliveryInfo = order.DeliveryInfo
+        };
+    }
+
+    public static ReserveInventoryDto ToReserveInventoryDto(this Order order)
+    {
+        return new ReserveInventoryDto
+        {
+            OrderId = order.OrderId,
+            ItemsInfo = order.ItemsInfo
+        };
+    }
+
+    public static ProcessPaymentDto ToProcessPaymentDto(this Order order)
+    {
+        return new ProcessPaymentDto
+        {
+            OrderId = order.OrderId,
+            PaymentInfo = order.PaymentInfo
+        };
+    }
+}
